2317c12d38da44262899d040dec937bd39457d8f
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2023-01-24 21:28:38 -0300

    Adding logic to play the game

--- src/application/program.java
+++ src/application/program.java
@@ -2,12 +2,29 @@ package application;
 
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 public class program {
 
     public static void main(String[] args) {
-       ChessMatch ch = new ChessMatch();
-       
-       UI.printBoard(ch.getPieces());
+        ChessMatch ch = new ChessMatch();
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            UI.printBoard(ch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            
+            System.out.println("");
+            System.out.print("Target:");
+            
+            ChessPosition target = UI.readChessPosition(sc);
+            
+            ChessPiece capturedPiece = ch.performChessMove(source, target);
+        }
     }
 }
