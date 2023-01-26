923f6c452bad295d7d8c0d538bd33b463deab522
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2023-01-25 21:35:38 -0300

    Adding the defensive code for program.java

--- src/application/program.java
+++ src/application/program.java
@@ -1,31 +1,41 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class program {
 
     public static void main(String[] args) {
         ChessMatch ch = new ChessMatch();
-        
+
         Scanner sc = new Scanner(System.in);
-        
+
         while (true) {
-            UI.clearScreen();
-            UI.printBoard(ch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
-            
-            System.out.println("");
-            System.out.print("Target:");
-            
-            ChessPosition target = UI.readChessPosition(sc);
-            
-            ChessPiece capturedPiece = ch.performChessMove(source, target);
+            try {
+                UI.clearScreen();
+                UI.printBoard(ch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
+
+                System.out.println("");
+                System.out.print("Target:");
+
+                ChessPosition target = UI.readChessPosition(sc);
+
+                ChessPiece capturedPiece = ch.performChessMove(source, target);
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
     }
 }
