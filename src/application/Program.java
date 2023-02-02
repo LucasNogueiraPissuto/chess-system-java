7b6eed2c15b31db8b4d1ae4ea6c9bf242734c7e9
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2023-02-01 22:48:01 -0300

    Adding the captured pieces in main program

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,20 +4,23 @@ import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
+
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
         ChessMatch ch = new ChessMatch();
-
         Scanner sc = new Scanner(System.in);
+        List<ChessPiece> captured = new ArrayList<>();
 
         while (true) {
             try {
                 UI.clearScreen();
-                UI.printMatch(ch);
+                UI.printMatch(ch, captured);
                 System.out.println();
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
@@ -33,6 +36,10 @@ public class Program {
                 ChessPosition target = UI.readChessPosition(sc);
 
                 ChessPiece capturedPiece = ch.performChessMove(source, target);
+
+                if (capturedPiece != null){
+                    captured.add(capturedPiece);
+                }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
                 sc.nextLine();
