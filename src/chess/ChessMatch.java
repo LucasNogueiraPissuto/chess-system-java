8d11ac4506b5e4ecfef856a704e19accf31e6b34
Lucas Pissuto <lucasnogueirapissuto@gmail.com>
2023-02-14 19:37:38 -0300

    Adding the new Special Move an passant

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -128,6 +128,22 @@ public class ChessMatch {
             rook.increaseMoveCount();
         }
 
+        //Special Move an passant
+        if (p instanceof Pawn){
+            if (source.getColumn() != target.getColumn() && capturedPiece == null){
+                Position pawnPosition;
+                if(p.getColor() == Color.WHITE){
+                    pawnPosition = new Position(target.getRow() + 1, target.getColumn());
+                }
+                else {
+                    pawnPosition = new Position(target.getRow() - 1, target.getColumn());
+                }
+                capturedPiece = board.removePiece(pawnPosition);
+                capturedPieces.add(capturedPiece);
+                piecesOnTheBoard.remove(capturedPiece);
+            }
+        }
+
         return capturedPiece;
     }
 
@@ -159,6 +175,23 @@ public class ChessMatch {
             board.placePiece(rook, sourceT);
             rook.decreaseMoveCount();
         }
+
+        //Special Move an passant
+        if (p instanceof Pawn){
+            if (source.getColumn() != target.getColumn() && capturedPiece == enPassanVulnerable){
+                ChessPiece pawn = (ChessPiece)board.removePiece(target);
+                Position pawnPosition;
+                if(p.getColor() == Color.WHITE){
+                    pawnPosition = new Position(3, target.getColumn());
+                }
+                else {
+                    pawnPosition = new Position(4, target.getColumn());
+                }
+
+                board.placePiece(pawn, pawnPosition);
+            }
+        }
+
 
     }
     
